52e31f93cedb7f64304aca33bb4c5a37588fa849
QAScholar <qa.scholar@yandex.ru>
2017-11-21 16:26:34 +1000

    testing_system interface

---
+++ src/Interface_testing_system/questions.java
@@ -0,0 +1,19 @@
+package Interface_testing_system;
+
+public class questions implements testing_system {
+
+
+
+    @Override
+    public void askquestion() {
+        System.out.println("What is Java?");
+
+
+    }
+
+}
+
+
+
+
+
