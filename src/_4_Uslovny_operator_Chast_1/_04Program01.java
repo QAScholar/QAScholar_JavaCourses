826a1cf5c771317017c7c877676ad88faf2f5448
QAScholar <qa.scholar@yandex.ru>
2017-11-22 12:03:02 +1000

    4_Uslovny_operator_Chast_1

--- src/_4_Uslovny_operator_Chast_1/_04Program01.java
+++ src/_4_Uslovny_operator_Chast_1/_04Program01.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 //1.	Ввести число, и определить – чётное оно, или нет.
 public class _04Program01 {
     public static void main(String[] args) {
+       //Вводим число
         System.out.println("Enter a number: ");
         Scanner a=new Scanner(System.in);
         int x=a.nextInt();
+        //Определяем четное или нет
         if (x%2==0){
             System.out.println("The number is even");
         }
