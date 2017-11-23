26b2a91890d0eeff838d19be9a9cbc8a27a7eaf8
QAScholar <qa.scholar@yandex.ru>
2017-11-23 16:35:13 +1000

    4_Uslovny_operator_Chast_1

--- src/_06_Tsikly_Nachalo/_06Program02.java
+++ src/_06_Tsikly_Nachalo/_06Program02.java
@@ -1,5 +1,5 @@
 package _06_Tsikly_Nachalo;
-
+//2.	Написать программу, выводящую на экран только четные целые числа из диапазона от 1 до 100.
 public class _06Program02 {
     public static void main(String[] args) {
         for (int i=2;i<=100;i=i+2)
